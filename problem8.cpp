28f7570912c0977dab12f4e604e39985afc3c3b9
250072-dot <250072@newuu.uz>
2025-11-20 12:54:46 +0500

    changed

---
+++ problem8.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+
+void reversearr(int** arr, int size) {
+
+    for (int i = 0; i < size; i++) {
+        cout << *(arr + size-(1+i))<<" ";
+
+    }
+}
+int main() {
+
+    int array[5] = {1,2,3,4,95};
+    int *p1 = &array[0];
+    int **p2 = &p1;
+
+    }
+
+    reversearr(*p2, 5);
+}
